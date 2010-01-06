678696985e18bbb5841f808bb93c6746cf722461
Matt Patterson <matt@reprocessed.org>
2010-01-06 19:14:38 +0000

    explicitly calling template.verify_rendered causes the expected failure with Mocha

--- spec/views/rendering/that_action.html.erb_spec.rb
+++ spec/views/rendering/that_action.html.erb_spec.rb
@@ -1,9 +1,17 @@
 require 'spec_helper'
 
 describe "/rendering/that_action.html.erb" do
-  it "should render a partial" do
+  it "should render a partial" do # Fails to fail under Mocha
     template.should_receive(:render).with(:partial => 'my_partial')
     
     render
+  end
+  
+  it "should render a partial" do # Fails under Mocha
+    template.should_receive(:render).with(:partial => 'my_partial')
+    
+    render
+    
+    template.verify_rendered
   end
 end
